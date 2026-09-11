852bc543240d65438a7b5a6e16d8dc6bcb3e415a
sebastian.veras <sebastian.veras@udea.edu.co>
2026-09-11 17:31:13 -0500

    se agrega parte a las funciones y se corrigen funciones

--- desafio-1/crush.cpp
+++ desafio-1/crush.cpp
@@ -1,37 +1,43 @@
 #include "crush.h"
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-int main()
-{
-    srand(time(NULL));
 
-    int calcularbytesnecesarios (int filas , int columnas);
+
+using namespace std;
+
+
+
+int calcularbytesnecesarios (int filas , int columnas) {
         int totalbits = filas * columnas * 3;
     return (totalbits + 7) /8;
 
-        int bitsactuales = calcularbytesnecesarios (filas ,columnas);
-unsigned char* guardarfichas = ( unsigned char* memoria,int columnas, int filas,int columna, int valor);
+unsigned char* guardarfichas ( unsigned char* memoria,int columnas, int filas,int columna, int valor){
 valor =  valor & 7 ;
- int indiceficha = fila* columnas + columna;
+ int indiceficha = filas * columnas + columna;
 int bitinicial = indiceficha * 3;
  int numerobytes = bitinicial/8;
 int posicionenbyte = bitinicial % 8;
 
- if (posicionenbyte <= 5); {
-     return (memoria[numerobytes] >> posicionenbyte) & 7;
-
-     else if ( posicionenbyte == 6) {
-         int parte1 = memoria [numerobytes]>> 6 ) & 3
-
-
-
-     }
-
-
+ if (posicionenbyte <= 5) {
+    (memoria[numerobytes] &= ~(7 << posicionenbyte);
  }
+    else if ( posicionenbyte == 6) {
+    int parte1 = valor & 3;
+    int parte2 = (valor >> 2) & 1;
+        (memoria[numerobytes] &= ~(3 << 6);
+        (memoria[numerobytes] |= (parte1 << 6);
+
+        (memoria[numerobytes + 1] &= ~1;
+        (memoria[numerobytes + 1] |= parte2;
+    }
+        else {
+    int parte1 = valor & 1;
+    int parte2 = (valor >> 1 ) & 3;
+                int parte2 = (memoria [numerobytes + 1] & 3 )<< 1;
+        return parte1 | parte2;
 
 
+
+        }
+}
 
 }
--- desafio-1/crush.h
+++ desafio-1/crush.h
@@ -1,4 +1,8 @@
 #ifndef CRUSH_H
 #define CRUSH_H
+
 #include <iostream>
-#endif // CRUSH_H
+
+int calcularbytesnecesarios (int filas , int columnas);
+int guardarFicha(unsigned char* memoria, int columnas, int fila, int columna);
+#endif
