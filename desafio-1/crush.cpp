b2f58064378f3456fc879f4bcc0055237c68b702
sebastian.veras <sebastian.veras@udea.edu.co>
2026-09-10 20:59:42 -0500

    se agregan funcuiones

--- desafio-1/crush.cpp
+++ desafio-1/crush.cpp
@@ -5,8 +5,33 @@
 using namespace std;
 int main()
 {
+    srand(time(NULL));
+
     int calcularbytesnecesarios (int filas , int columnas);
         int totalbits = filas * columnas * 3;
     return (totalbits + 7) /8;
+
+        int bitsactuales = calcularbytesnecesarios (filas ,columnas);
+unsigned char* guardarfichas = ( unsigned char* memoria,int columnas, int filas,int columna, int valor);
+valor =  valor & 7 ;
+ int indiceficha = fila* columnas + columna;
+int bitinicial = indiceficha * 3;
+ int numerobytes = bitinicial/8;
+int posicionenbyte = bitinicial % 8;
+
+ if (posicionenbyte <= 5); {
+     return (memoria[numerobytes] >> posicionenbyte) & 7;
+
+     else if ( posicionenbyte == 6) {
+         int parte1 = memoria [numerobytes]>> 6 ) & 3
+
+
+
+     }
+
+
+ }
+
+
 
 }
