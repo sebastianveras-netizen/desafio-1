ed13efc2d664fa6aebe3515b62b6acdd9a11adbc
Diego A. Cordoba <95111153+DiegoCord@users.noreply.github.com>
2026-09-16 14:28:10 -0500

    se colocan comentarios claves y explicacion de algunos segmentos

--- desafio-1/main.cpp
+++ desafio-1/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int fila=0, columna=0, seleccion_fila=0, seleccion_columna=0, eliminacionesusuario=0;
+    int fila=0, columna=0, seleccion_fila=0, seleccion_columna=0, eliminacionesusuario=0;  //declaaramos las variables a utilizar
 
 
 
@@ -21,22 +21,27 @@ int main()
     cin>>columna;
 
 
-    //funcion sebas
-
-    int bytesactuales= calcularbytesnecesarios(fila, columna);
-
-    unsigned char* memoria= new unsigned char[bytesactuales]() ;
 
 
+    int bytesactuales= calcularbytesnecesarios(fila, columna); //lo datos suministrados van para funcion calcularbytesnecesarios
 
-    for (int i = 0; i < fila ; ++i) {
-        for (int c = 0; c < columna; ++c) {
-            int fichaaleatoria= rand()%6;
-            memoria= guardarficha(memoria, columna, i, c, fichaaleatoria);
+    unsigned char* memoria= new unsigned char[bytesactuales]();
+    //declaramos un puntero y reservamos espacio en memoria dinamico en el heap en donde vamos a guardar los valores de bytesactuales
+
+
+
+
+    for (int i = 0; i < fila ; ++i) {//recorre en cada interacion las filas
+        for (int c = 0; c < columna; ++c) {//recorre en cada interacion las columnas
+            int fichaaleatoria= rand()%6; //aqui se generara un numero aleatorio entre 0 y 5 para luego poder definir la ficha
+            memoria= guardarficha(memoria, columna, i, c, fichaaleatoria);// se llama funcion guardarficha en donde los valores de entrada iran cambiando en cada interacion
+
         }
 
 
-    }
+    }  
+//se recorre todas las celdas i-filas c-columnas
+
 
 // aqui inician las opciones
 
@@ -53,15 +58,18 @@ int main()
 
 
          if (opcion ==1){
-             memoria =imprimirtablerofichas(memoria,fila, columna);
+             memoria =imprimirtablerofichas(memoria,fila, columna);//llamamos esta funcion para traer los valores almacenados de las fichasaleatorias
 
          }
          else if(opcion ==2){
-             memoria =imprimirtablerobinario(memoria,fila,columna);
+             memoria =imprimirtablerobinario(memoria,fila,columna);/*cuando se ejecutamos esta opcion esta imprime la representacion directa
+                                                                       en bits o numeros binarios que almacenamos en memoria*/
          }
 
 
          else if(opcion==3){
+
+             /*seleccionamos las coordenadas fila y columnas*/
 
              cout<<"coloca la fila: "<<endl;
                      cin>>seleccion_fila;
@@ -69,6 +77,9 @@ int main()
              cout<<"coloca la columna: "<<endl;
                      cin>>seleccion_columna;
 
+                     /*se evalua 4 condiciones con && con lo que no aseguramos que todas deben ser verdaderas, donde no dejamos ingrear
+                      indices negativos y que la selecion de filas y columnas no superen el tamaño del tablero, si todo cupmple se llama la fincion
+                       la funcion guardarficha.  */
 
                      if (seleccion_fila>=0 && seleccion_fila < fila && seleccion_columna>=0 &&seleccion_columna < columna ){
 
@@ -77,7 +88,7 @@ int main()
                          cout<<"se elimino la ficha";
 
                      }else{
-                         cout<<"no se encuentra la posicion";
+                         cout<<"no se encuentra la posicion"; //se activa cuando la selecion de fila como columnas no coinciden con el tablero
 
                      }
 
