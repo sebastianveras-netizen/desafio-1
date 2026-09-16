8490fbcd6b562d00e394f71ba1119dcf83af617a
sebastianveras-netizen <sebastian.veras@udea.edu.co>
2026-09-16 18:06:34 -0500

    Sebastian (#19)
    * Sebastian (#16)
    
    * se agrega parte a las funciones y se corrigen funciones
    
    * se agrega documento informe parcial
    
    * se agrega documento informe preliminar con imagen
    
    * agregamos funciones nuevas
    
    * se agregan funciones para el menu
    
    * se comentan las funciones
    
    * se comentan mas funciones
    
    * Diego (#17)
    
    * se agregan funcuiones (#3) (#5)
    
    Co-authored-by: sebastianveras-netizen <sebastian.veras@udea.edu.co>
    
    * opciones_completas, correccion de llamado de libreria, falta correccion de funciones en el apartado de opcion
    
    * 1. se corrige el nombramiento de funciones; 2. se elimina un salto de linea en el archivo crush cpp
    
    * se colocan comentarios claves y explicacion de algunos segmentos
    
    ---------
    
    Co-authored-by: sebastianveras-netizen <sebastian.veras@udea.edu.co>
    Co-authored-by: Diego A. Cordoba <95111153+DiegoCord@users.noreply.github.com>
    
    * se resuelve conflicto nuevamente
    
    ---------
    
    Co-authored-by: dalejandrocordoba-sudo <dalejandro.cordoba@udea.edu.co>
    Co-authored-by: Diego A. Cordoba <95111153+DiegoCord@users.noreply.github.com>

--- desafio-1/main.cpp
+++ desafio-1/main.cpp
@@ -9,11 +9,7 @@ using namespace std;
 int main()
 {
     srand(time(0));
-<<<<<<< HEAD
-    int fila=0, columna=0, seleccion_fila=0, seleccion_columna=0, eliminacionesusuario=0;  //declaaramos las variables a utilizar
-=======
     int fila=0, columna=0, seleccion_fila=0, seleccion_columna=0, eliminacionesusuario=0;
->>>>>>> 60d3378d0ceec035b72b9d4e8ed422c17f627c3a
 
 
 
@@ -43,17 +39,11 @@ int main()
         }
 
 
-<<<<<<< HEAD
-    }  
-//se recorre todas las celdas i-filas c-columnas
-
-=======
     }
->>>>>>> 60d3378d0ceec035b72b9d4e8ed422c17f627c3a
 
 // aqui inician las opciones
 
-    int opcion= -1;
+int opcion= -1;
     while (opcion!=0){
         cout<<"elije una de las siguientes opciones"<<endl;
 
@@ -66,20 +56,11 @@ int main()
 
 
          if (opcion ==1){
-<<<<<<< HEAD
-             memoria =imprimirtablerofichas(memoria,fila, columna);//llamamos esta funcion para traer los valores almacenados de las fichasaleatorias
-
-         }
-         else if(opcion ==2){
-             memoria =imprimirtablerobinario(memoria,fila,columna);/*cuando se ejecutamos esta opcion esta imprime la representacion directa
-                                                                       en bits o numeros binarios que almacenamos en memoria*/
-=======
              memoria =imprimirtablerofichas(memoria,fila, columna);
 
          }
          else if(opcion ==2){
              memoria =imprimirtablerobinario(memoria,fila,columna);
->>>>>>> 60d3378d0ceec035b72b9d4e8ed422c17f627c3a
          }
 
 
@@ -101,14 +82,10 @@ int main()
 
                          memoria= guardarficha(memoria, columna, seleccion_fila, seleccion_columna, 6);
                          eliminacionesusuario++;
-                         cout<<"se elimino la ficha";
+                         cout<<"se elimino la ficha" <<endl;
 
                      }else{
-<<<<<<< HEAD
-                         cout<<"no se encuentra la posicion"; //se activa cuando la selecion de fila como columnas no coinciden con el tablero
-=======
                          cout<<"no se encuentra la posicion";
->>>>>>> 60d3378d0ceec035b72b9d4e8ed422c17f627c3a
 
                      }
 
