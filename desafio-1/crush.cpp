dac3f2487f1d7fb08c69098746500b17d06fc3cc
sebastian.veras <sebastian.veras@udea.edu.co>
2026-09-16 21:18:10 -0500

    se agrega la funcion redimencionar

--- desafio-1/crush.cpp
+++ desafio-1/crush.cpp
@@ -129,7 +129,7 @@ char obtenercaracter(int valor){
 
 unsigned char* imprimirtablerofichas ( unsigned char* memoria, int filas,int columnas) {
 
-    cout << "TABLERO" << endl;
+    cout << "      TABLERO     " << endl;
     for (int f=0;f<filas; ++f) {
             cout << " FILA "<< f << " ";/*va a imprimir en que fila esta cada una empezando desde 0*/
     for (int c=0;c<columnas;++c){
@@ -166,7 +166,41 @@ return memoria;
 }
 
 
+unsigned char* redimencionar ( unsigned char* memoria,int filas_actuales, int columnas_actuales,int filas_nuevas, int columnas_nuevas,int eliminaciones){
+    int total_casillas= filas_actuales*columnas_actuales;/*verificamos la cantidad de fichas que se implementaron en un principio*/
+    int casillas_activas = total_casillas - eliminaciones;/*le restamos la cantidad de fichas eliminadas*/
 
+    double porcentaje=((double)casillas_activas/total_casillas)*100.0;/*sacamos el porcentaje que queda de fichas*/
+    if (porcentaje < 65.0){/*si el porcentaje es inferior a 65% entra */
+        delete [] memoria;/*eliminamos la memoria dinamica que existe en la variable memoria*/
+
+        int nuevos_bytes = calcularbytesnecesarios(filas_nuevas,columnas_nuevas);/*verificamos cuantos bytes requiere nuestra cuadricula
+                                                                                de acuerdo a nuestras nuevas dimenciones*/
+        unsigned char* nueva_memoria = new unsigned char [nuevos_bytes]();/*asignamos un puntero a nuestra nueva memoria con un arreglo
+                                                                        que varia de acuerdo a la cantidad de bytes que hayamos calculado
+                                                                        con la funcion anterior*/
+
+        for(int f=0; f < filas_nuevas; ++f ){/*realizamos un recorridos fila por fila */
+            for (int c=0;c< columnas_nuevas; ++c){/*entramos en cada columna de las diferentes filas*/
+                int fichaaleatoria= rand() %6;/*asignamos un numero aleatorio gracias a la semilla que implementa rand y aplicamos modulo 6
+                                            para limitar la cantidad de letras a un maximo de 5 siendo desde la A hasta la F*/
+                nueva_memoria = guardarficha(nueva_memoria,columnas_nuevas,f,c,fichaaleatoria);/*de acuerdo a la fila y columna en la que
+                                                                                entramos guardamos una determinada ficha que sera almacenada
+                                                                                de acuerdo a ala estructura y cantidad de cada byte siendo
+                                                                                explicado en la funcion guardarficha */
+
+            }
+
+
+        }
+
+        cout << "redimensionamiento completado dimencion antigua"<< porcentaje << "% " <<endl;
+        return nueva_memoria;/*se almacena la informacion en nueva memoria para poder ser invocado mas adelante*/
+
+    }
+    cout << "no puede ser redimencionado el porcentaje es "<< porcentaje << "% el porcentaje debe ser inferior a 65%"<<endl;
+    return memoria;/*en caso de que no se redimencione simplemente retorna la memoria original*/
+}
 
 
 
